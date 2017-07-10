9f7db20273f5826aa6baa7b243a7087759de9795
Mahesh <maheshpati8992@gmail.com>
2017-07-10 15:06:38 -0700

    Added proper routes and refactored db logic

--- customer_api.py
+++ customer_api.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request
 import customerDao
+
 app = Flask(__name__)
 
 
-@app.route('/customer/', methods=['GET'])
-def get_customer_info():
-    customer_name = 'Jai Ambe'
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/order', methods=['POST'])
+def order():
+    customer_name = request.form['customer_name']
+    date = request.form['date']
+    customer_details = customerDao.get_customer(customer_name)
+    print("details", customer_name, date)
+    print(customer_details)
+    return render_template('order_page.html')
+
+
+# @app.route('/customer/<customer_id>', methods=['GET'])
+@app.route('/customer/<customer_name>', methods=['GET'])
+def get_customer_info(customer_name):
     customer = customerDao.get_customer(customer_name)
     return 'Hello'
 
@@ -24,4 +41,4 @@ def add_new_customer():
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run(host='0.0.0.0')
