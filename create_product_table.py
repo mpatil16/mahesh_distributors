1dd058d954e9110321a5fb3c00caebd752a1b264
Mahesh <maheshpatil8992@gmail.com>
2017-07-31 00:27:39 +0530

    Added new products in table script

--- create_product_table.py
+++ create_product_table.py
@@ -3,21 +3,25 @@ import sqlite3
 
 conn = sqlite3.connect('test.db')
 
-conn.execute('''drop table Records''')
+conn.execute('''drop table Products''')
 # print('Table Dropped Successfully')
 
-conn.execute('''CREATE TABLE Records(customer_name varchar(50), date date, 
+conn.execute('''CREATE TABLE Products(customer_name varchar(50), date date,
             gst_number varchar(20), shrikhand_50 number(5), shrikhand_100
             number(5), shrikhand_250 number(5), shrikhand_500 number(5),
-            amrakhand_50 number(5), amrakhand_100 number(5), amrakhand_250
-            number(5), amrakhand_500 number(5), lassi_200 number(5), curd_50 
-            number(5), curd_100 number(5), curd_200 number(5), curd_400 
-            number(5), buffalo_200 number(5), buffalo_500 number(5), 
-            buffalo_1000 number(5), cow_200 number(5), cow_500 number(5), 
+            shrikhand_loose number(5), amrakhand_50 number(5), amrakhand_100
+            number(5), amrakhand_250 number(5), amrakhand_500 number(5),
+            amrakhand_loose number(5), keshar_250 number(5), keshar_500
+            number(5), lassi_200 number(5), tetra_lassi number(5), curd_50
+            number(5), curd_100 number(5), curd_200 number(5), curd_400
+            number(5), buffalo_200 number(5), buffalo_500 number(5),
+            buffalo_1000 number(5), cow_200 number(5), cow_500 number(5),
             cow_1000 number(5), butter_100 number(5), butter_500 number(5), 
             paneer_200 number(5), paneer_500 number(5), plain_tak number(5),
-            aqua_water number(5), tetra_tak number(5), tetra_lassi number(5));''')
+            masala_tak number(5), tetra_tak number(5), aqua_water number(5),
+            flavoured number(5), juice number(5), cheese number(5), smp number(5));''')
 
+# There are 37 products and 3 info attributes(39 in total).
 print("Table created successfully")
 
 conn.close()
